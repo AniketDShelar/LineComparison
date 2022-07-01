023002589a0f7849198e450b9796d96fa68edd41
Aniket Shelar <theaniketshelar@gmail.com>
2022-07-01 13:48:54 +0530

    REFACTOR: use case 6

--- src/com/bridgelabz/employeewage/Empwage.java
+++ src/com/bridgelabz/employeewage/Empwage.java
@@ -5,24 +5,31 @@ public class Empwage {
 	public static final int IS_PART_TIME = 2;
 	public static final int wageperhour = 20;
 	public static final int NUM_OF_WORKING_DAYS = 20;
-	public static void main(String[] args) {
-		int emphrs = 0, empwage=0, totalempwage = 0;
-		for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
-		int empcheck = (int)Math.floor(Math.random() * 10) % 3;
-		switch (empcheck) {
-		case IS_FULL_TIME:
-		emphrs = 8;
-		break;
-		case IS_PART_TIME:
-			emphrs = 4;
-			break;
-		default:
-		emphrs = 0;
+	public static final int MAX_WORKING_HRS = 100;
+
+	public static void main(String[] args) {;
+		int emphrs = 0, empwage = 0, totalempwage = 0, totalworkinghrs = 0, totalworkingdays = 0;
+		while (totalworkinghrs <= MAX_WORKING_HRS && totalworkingdays < NUM_OF_WORKING_DAYS) {
+			totalworkingdays ++;
+			int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empcheck) {
+			case IS_FULL_TIME:
+				emphrs = 8;
+				break;
+			case IS_PART_TIME:
+				emphrs = 4;
+				break;
+			default:
+				emphrs = 0;
+			}
+			totalworkinghrs += emphrs;
+			for (int i = 0; i <= 20; i++)
+			System.out.println("Day " +i+ " working hour = " +emphrs);
 		}
-		  empwage = emphrs * wageperhour;
-		  totalempwage += empwage;
-		  System.out.println("day "+day+"Employeewage = "+empwage);
-		}
-			System.out.println("Total Employee wage = "+totalempwage);
-    }
+		empwage = totalworkinghrs * wageperhour;
+		totalempwage += empwage;
+		System.out.println("total days = "+totalworkingdays);
+		System.out.println("Employeewage = " + empwage);
+		System.out.println("Total Employee wage = " + totalempwage);
+	}
 }
