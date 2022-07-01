0b8678650d8aec292776ffcbf77396d8a0715d03
Aniket Shelar <theaniketshelar@gmail.com>
2022-07-01 13:38:12 +0530

    REFACTOR: use case 3

--- src/com/bridgelabz/employeewage/Empwage.java
+++ src/com/bridgelabz/employeewage/Empwage.java
@@ -1,18 +1,25 @@
 package com.bridgelabz.employeewage;
 
 public class Empwage {
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int wageperhour = 20;
+
 	public static void main(String[] args) {
-		int IS_PRESENT = 1;
-		int wageperhour = 20;
-		int totalworkinghrs = 8;
-		int empwage;
-	    double empcheck = Math.floor(Math.random() * 10) % 2;
-		if (empcheck == IS_PRESENT) {
-			System.out.println("Employee is present");
-			empwage = wageperhour * totalworkinghrs;
-			System.out.println("Employee wage = " +empwage);
-	    }else {
-	    	System.out.println("Employee is absent");
-	    }
+		int emphrs = 0;
+		int empwage = 0;
+		int empcheck =(int)Math.floor(Math.random() * 10) % 3;
+		switch (empcheck) {
+		case IS_FULL_TIME:
+		emphrs = 8;
+		break;
+		case IS_PART_TIME:
+			emphrs = 4;
+			break;
+		default:
+		emphrs = 0;
+		}
+		  empwage = emphrs * wageperhour;
+		  System.out.println("Employee wage is = "+empwage);
 	}
 }
