30e26137799eb2d624be41d2c687e34fbe56fae8
Aniket Shelar <theaniketshelar@gmail.com>
2022-07-01 13:46:26 +0530

    REFACTOR: use case 5

--- src/com/bridgelabz/employeewage/Empwage.java
+++ src/com/bridgelabz/employeewage/Empwage.java
@@ -4,22 +4,25 @@ public class Empwage {
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 	public static final int wageperhour = 20;
-
+	public static final int NUM_OF_WORKING_DAYS = 20;
 	public static void main(String[] args) {
-		int emphrs = 0;
-		int empwage = 0;
-		double empcheck = Math.floor(Math.random() * 10) % 3;
-		if (empcheck == IS_FULL_TIME) {
-			emphrs = 8;
-			System.out.println("Employee is present as full time");
-		} else if (empcheck == IS_PART_TIME) {
+		int emphrs = 0, empwage=0, totalempwage = 0;
+		for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+		int empcheck = (int)Math.floor(Math.random() * 10) % 3;
+		switch (empcheck) {
+		case IS_FULL_TIME:
+		emphrs = 8;
+		break;
+		case IS_PART_TIME:
 			emphrs = 4;
-			System.out.println("Employee is present as part time");
-		} else {
-			emphrs = 0;
-			System.out.println("Employee is absent");
+			break;
+		default:
+		emphrs = 0;
 		}
 		  empwage = emphrs * wageperhour;
-		  System.out.println("Employee wage is = "+empwage);
-	}
+		  totalempwage += empwage;
+		  System.out.println("day "+day+"Employeewage = "+empwage);
+		}
+			System.out.println("Total Employee wage = "+totalempwage);
+    }
 }
