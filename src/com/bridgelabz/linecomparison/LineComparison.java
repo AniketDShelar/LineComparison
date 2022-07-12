eec88f2f5f0741bcc6fd8be3492db52f4f2b122f
Aniket Shelar <theaniketshelar@gmail.com>
2022-07-12 16:09:49 +0530

    Refactored: use of equals to method to compare two lines

--- src/com/bridgelabz/linecomparison/LineComparison.java
+++ src/com/bridgelabz/linecomparison/LineComparison.java
@@ -1,25 +1,35 @@
 package com.bridgelabz.linecomparison;
 
-import java.util.*;
-
+import java.util.Scanner;
 public class LineComparison {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Length of all lines are give below ->");
-
-		System.out.print("Enter x1 = ");
-		int x1 = sc.nextInt();
-
-		System.out.print("Enter x2 = ");
-		int x2 = sc.nextInt();
-
-		System.out.print("Enter y1 = ");
-		int y1 = sc.nextInt();
-
-		System.out.print("Enter y2 = ");
-		int y2 = sc.nextInt();
-
-		double length = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-		System.out.println("Length of line = " +length);
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter x1 : ");
+		int x1 = scanner.nextInt();
+		System.out.println("Enter x2 : ");
+		int x2 = scanner.nextInt();
+		System.out.println("Enter y1 : ");
+		int y1 = scanner.nextInt();
+		System.out.println("Enter y2 : ");
+		int y2 = scanner.nextInt();
+		
+		System.out.println("Enter x3 : ");
+		int x3=scanner.nextInt();
+		System.out.println("Enter x4 : ");
+		int x4=scanner.nextInt();
+		System.out.println("Enter y3 : ");
+		int y3=scanner.nextInt();
+		System.out.println("Enter y4 : ");
+		int y4=scanner.nextInt();
+		Double lineLength1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		Double lineLength2 = Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
+		System.out.println(" Length of line 1 is  : "+lineLength1);
+		System.out.println(" Length of line 2 is  : "+lineLength2);
+		//used equals method 
+		if(lineLength1.equals(lineLength2)) {
+			System.out.println(" Both Lines are equals ");
+		}else {
+			System.out.println(" Both Lines are not equals ");
+		}
 	}
 }
